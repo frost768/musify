14477b6f5c981ca33c5146de03d9ae3f98239505
frost768 <droidplus99@gmail.com>
2021-07-18 18:57:02 +0300

    Add constants and main

---
+++ lib/consts.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+const kMainColor = Color(0xff202020);
+const kPlayerNarrowHeight = 70.0;
+const kBottomSideItemHeight = 60.0;
+const kBottomSideTotalHeight = 132.0;
+const kMainBackColor = Color(0xff171717);
---
+++ lib/main.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:spotify_clone/components/player_narrow.dart';
+import 'package:spotify_clone/views/views.dart';
+
+void main() => runApp(MyApp());
+
+final List<Widget> _pages = [HomeView(), SearchView(), LibraryView()];
+
+class MyApp extends StatelessWidget {
+  final PageController _pageController = PageController();
+  void onPageChange(int index) {
+    _pageController.jumpToPage(index);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: kMainColor),
+      themeMode: ThemeMode.dark,
+      home: Scaffold(
+        backgroundColor: kMainBackColor,
+        body: PageView(
+          children: _pages,
+          controller: _pageController,
+          onPageChanged: onPageChange,
+        ),
+        bottomNavigationBar: _buildBottomBar,
+      ),
+    );
+  }
+
+  Widget get _buildBottomBar {
+    var _icons = [
+      IconButton(
+          onPressed: () => onPageChange(0),
+          icon: Icon(
+            Icons.home,
+            color: Colors.white,
+          )),
+      IconButton(
+          onPressed: () => onPageChange(1),
+          icon: Icon(
+            Icons.search,
+            color: Colors.white,
+          )),
+      IconButton(
+          onPressed: () => onPageChange(2),
+          icon: Icon(
+            Icons.library_music_outlined,
+            color: Colors.white,
+          )),
+    ];
+    return SizedBox(
+      height: kBottomSideTotalHeight,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          PlayerNarrow(),
+          Divider(
+            height: 2,
+            color: kMainBackColor,
+          ),
+          BottomAppBar(
+            color: kMainColor,
+            child: SizedBox(
+              height: kBottomSideItemHeight,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: _icons,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
