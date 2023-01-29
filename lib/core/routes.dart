4997986ad8ef03a148f8a742e0e564e5d531a8a7
frost768 <droidplus99@gmail.com>
2023-01-29 15:33:13 +0300

    AlbumView moved back

--- lib/core/routes.dart
+++ lib/core/routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/cupertino.dart';
-import 'package:spotify_clone/core/album_view.dart';
+import 'package:spotify_clone/views/album_view.dart';
 import 'package:spotify_clone/views/views.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
