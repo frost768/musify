8a520dc81794aa656431af8498afaf59416bce28
frost768 <droidplus99@gmail.com>
2023-02-03 23:08:55 +0300

    get library removed switched to riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:spotify_clone/controllers/player_controller.dart';
-import 'package:spotify_clone/controllers/user_controller.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spotify_clone/core/app.dart';
 import 'package:spotify_clone/services/cache_service.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  CacheService.init();
-  Get.put(UserController());
-  Get.put(PlayerController());
-  runApp(SpotifyClone());
+  CacheService().init();
+  runApp(ProviderScope(child: SpotifyClone()));
 }
